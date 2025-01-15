51889a41b191cf72e0d44df5987cce15ced88e0d
SeHyun <rla6170@naver.com>
2025-01-15 16:43:40 +0900

    feat: reset layer -> global Layer 로 변경

--- src/styles/layers/layers.css.ts
+++ src/styles/layers/layers.css.ts
@@ -1,6 +1,5 @@
-import { layer } from '@vanilla-extract/css'
+import { globalLayer, layer } from '@vanilla-extract/css'
 
-export const reset = layer('reset')
+export const reset = globalLayer('reset')
 export const components = layer('components')
-
 // layer service 지원 브라우저 https://caniuse.com/css-cascade-layers
