2db62ceab07cc0e0ee854b59bcc114c86c11e99a
SeHyun <rla6170@naver.com>
2025-01-16 15:59:12 +0900

    feat: css.ts 파일 prettier 적용

--- src/styles/sprinkle.css.ts
+++ src/styles/sprinkle.css.ts
@@ -1,49 +1,54 @@
-import { createSprinkles, defineProperties } from "@vanilla-extract/sprinkles";
-import { colors, fontSizes, fontWeights } from "./token";
+import { createSprinkles, defineProperties } from '@vanilla-extract/sprinkles'
+import { colors, fontSizes, fontWeights } from './token'
 
 /**
  * responsiveProperties : Box 모델과 관련된 CSS 속성을 정의
  */
 const responsiveProperties = defineProperties({
-    conditions : {
+    conditions: {
         mobile: {},
-        tablet: { "@media": "screen and (min-width: 768px)" },
-        desktop: { "@media": "screen and (min-width: 1024px)" }
+        tablet: { '@media': 'screen and (min-width: 768px)' },
+        desktop: { '@media': 'screen and (min-width: 1024px)' },
     },
-    defaultCondition : 'desktop',
+    defaultCondition: 'desktop',
     properties: {
-        display : ['none', 'block', 'inline-block', 'flex', 'grid'],
-        flexDirection : ['row', 'column'],
-        justifyContent : ['center', 'flex-start', 'flex-end', 'space-between', 'space-around'],
-        alignItems : ['center', 'flex-start', 'flex-end', 'stretch'],
-        placeItems : ['center', 'flex-start', 'flex-end', 'stretch'],
-    }
-});
+        display: ['none', 'block', 'inline-block', 'flex', 'grid'],
+        flexDirection: ['row', 'column'],
+        justifyContent: [
+            'center',
+            'flex-start',
+            'flex-end',
+            'space-between',
+            'space-around',
+        ],
+        alignItems: ['center', 'flex-start', 'flex-end', 'stretch'],
+        placeItems: ['center', 'flex-start', 'flex-end', 'stretch'],
+    },
+})
 
 /**
  * colorProperties : 색상과 관련된 CSS 속성을 정의
  */
 const colorProperties = defineProperties({
-    conditions : {
-        lightMode : {},
+    conditions: {
+        lightMode: {},
     },
-    defaultCondition : 'lightMode',
+    defaultCondition: 'lightMode',
     properties: {
-        color : colors,
-        background : colors
-    }
-});
+        color: colors,
+        background: colors,
+    },
+})
 
 /**
  * typeScaleProperties : 텍스트와 관련된 CSS 속성을 정의
  */
 const typeScaleProperties = defineProperties({
     properties: {
-        fontSize : fontSizes,
-        fontWeight : fontWeights
-    }
+        fontSize: fontSizes,
+        fontWeight: fontWeights,
+    },
 })
-
 
 /**
  * sprinkles : Sprinkle 객체
@@ -52,9 +57,9 @@ export const sprinkles = createSprinkles(
     responsiveProperties,
     colorProperties,
     typeScaleProperties
-);
+)
 
 /**
  * Sprinkles : Sprinkle 객체 타입
  */
-export type Sprinkles = Parameters<typeof sprinkles>[0];
+export type Sprinkles = Parameters<typeof sprinkles>[0]
--- src/styles/token.ts
+++ src/styles/token.ts
@@ -1,105 +1,104 @@
-export const fonts = {
-} as const;
+export const fonts = {} as const
 
 export const fontSizes = {
-    'largeTitle' : {
-        fontSize : 26,
-        lineHeight : 32
-    }, 
-    'title1' : {
-        fontSize : 22,
-        lineHeight : 26
+    largeTitle: {
+        fontSize: 26,
+        lineHeight: 32,
     },
-    'title2' : {
-        fontSize : 17,
-        lineHeight : 22
-    }, 
-    'title3' : {
-        fontSize : 15,
-        lineHeight : 20
+    title1: {
+        fontSize: 22,
+        lineHeight: 26,
     },
-    'headline' : {
-        fontSize : 13,
-        lineHeight : 16
+    title2: {
+        fontSize: 17,
+        lineHeight: 22,
     },
-    'subHeadline' : {
-        fontSize : 11,
-        lineHeight : 14
+    title3: {
+        fontSize: 15,
+        lineHeight: 20,
     },
-    'body' :{
-        fontSize : 13,
-        lineHeight : 16
+    headline: {
+        fontSize: 13,
+        lineHeight: 16,
     },
-    'caption' : {
-        fontSize : 10,
-        lineHeight : 13
-    }
-} as const;
+    subHeadline: {
+        fontSize: 11,
+        lineHeight: 14,
+    },
+    body: {
+        fontSize: 13,
+        lineHeight: 16,
+    },
+    caption: {
+        fontSize: 10,
+        lineHeight: 13,
+    },
+} as const
 
 export const fontWeights = {
-    regular : '400',
-    medium : '500',
-    semibold : '600',
-    bold : '700'
-} as const;
+    regular: '400',
+    medium: '500',
+    semibold: '600',
+    bold: '700',
+} as const
 
 export const colors = {
-    // neutral 텍스트 기본 색상 
-    'neutral-0' : '#000000',
-    'neutral-100' : '#7a8699',
-    'neutral-200' : '#6b788e',
-    'neutral-300' : '#5d6b82',
-    'neutral-400' : '#505f79',
-    'neutral-500' : '#42526d',
-    'neutral-600' : '#354764',
-    'neutral-700' : '#243757',
-    'neutral-800' : '#15294b',
-    'neutral-900' : '#091e42',
+    // neutral 텍스트 기본 색상
+    'neutral-0': '#000000',
+    'neutral-100': '#7a8699',
+    'neutral-200': '#6b788e',
+    'neutral-300': '#5d6b82',
+    'neutral-400': '#505f79',
+    'neutral-500': '#42526d',
+    'neutral-600': '#354764',
+    'neutral-700': '#243757',
+    'neutral-800': '#15294b',
+    'neutral-900': '#091e42',
     // blue 파란색
-    'blue-50' : '#e6f0ff',
-    'blue-75' : '#96c0ff',
-    'blue-100' : '#6ba6ff',
-    'blue-200' : '#2b7fff',
-    'blue-300' : '#0065ff',
-    'blue-400' : '#0047b3',
-    'blue-500' : '#003e9c',
+    'blue-50': '#e6f0ff',
+    'blue-75': '#96c0ff',
+    'blue-100': '#6ba6ff',
+    'blue-200': '#2b7fff',
+    'blue-300': '#0065ff',
+    'blue-400': '#0047b3',
+    'blue-500': '#003e9c',
     // red 빨간색
-    'red-50' : '#f9e9e6',
-    'red-75' : '#e5a696',
-    'red-100' : '#da816b',
-    'red-200' : '#ca4b2b',
-    'red-300' : '#bf2600',
-    'red-400' : '#861b00',
-    'red-500' : '#751700',
+    'red-50': '#f9e9e6',
+    'red-75': '#e5a696',
+    'red-100': '#da816b',
+    'red-200': '#ca4b2b',
+    'red-300': '#bf2600',
+    'red-400': '#861b00',
+    'red-500': '#751700',
     // yellow 노란색
-    'yellow-50' : '#fff3e6',
-    'yellow-75' : '#ffcf96',
-    'yellow-100' : '#ffbc6b',
-    'yellow-200' : '#ff9f2b',
-    'yellow-300' : '#ff8b00',
-    'yellow-400' : '#b36100',
-    'yellow-500' : '#9c5500',
+    'yellow-50': '#fff3e6',
+    'yellow-75': '#ffcf96',
+    'yellow-100': '#ffbc6b',
+    'yellow-200': '#ff9f2b',
+    'yellow-300': '#ff8b00',
+    'yellow-400': '#b36100',
+    'yellow-500': '#9c5500',
     // green 초록색
-    'green-50' : '#e6f0ec',
-    'green-75' : '#96c0b2',
-    'green-100' : '#6ba693',
-    'green-200' : '#2b8064',
-    'green-300' : '#006644',
-    'green-400' : '#004730',
-    'green-500' : '#003e29',
+    'green-50': '#e6f0ec',
+    'green-75': '#96c0b2',
+    'green-100': '#6ba693',
+    'green-200': '#2b8064',
+    'green-300': '#006644',
+    'green-400': '#004730',
+    'green-500': '#003e29',
     // teal 청록색
-    'teal-50' : '#e6f4f6',
-    'teal-75' : '#96d0db',
-    'teal-100' : '#6bbdcb',
-    'teal-200' : '#2ba0b5',
-    'teal-300' : '#008da6',
-    'teal-400' : '#006374',
-    'teal-500' : '#005665'
-} as const;
+    'teal-50': '#e6f4f6',
+    'teal-75': '#96d0db',
+    'teal-100': '#6bbdcb',
+    'teal-200': '#2ba0b5',
+    'teal-300': '#008da6',
+    'teal-400': '#006374',
+    'teal-500': '#005665',
+} as const
 
 export const shadows = {
-    small : '0px 1px 2px rgba(0, 0, 0, 0.05)',
-    medium : '0px 4px 6px -1px rgba(0, 0, 0, 0.1), 0px 2px 4px -2px rgba(0, 0, 0, 0.1)',
-    large : '0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -4px rgba(0, 0, 0, 0.1)',
-    xlarge : '0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 8px 10px -6px rgba(0, 0, 0, 0.1)'
-} as const;
+    small: '0px 1px 2px rgba(0, 0, 0, 0.05)',
+    medium: '0px 4px 6px -1px rgba(0, 0, 0, 0.1), 0px 2px 4px -2px rgba(0, 0, 0, 0.1)',
+    large: '0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -4px rgba(0, 0, 0, 0.1)',
+    xlarge: '0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 8px 10px -6px rgba(0, 0, 0, 0.1)',
+} as const
