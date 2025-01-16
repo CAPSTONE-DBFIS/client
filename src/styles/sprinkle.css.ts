e7e2aecd6a0ac70f1eb1fd39ce11115f443bbe4b
SeHyun <rla6170@naver.com>
2025-01-16 15:58:48 +0900

    feat: colors, fonts, shadows 전역 사용을 위한 theme, 강제 사용을 위한 sprinkle 추가

---
+++ src/styles/sprinkle.css.ts
@@ -0,0 +1,60 @@
+import { createSprinkles, defineProperties } from "@vanilla-extract/sprinkles";
+import { colors, fontSizes, fontWeights } from "./token";
+
+/**
+ * responsiveProperties : Box 모델과 관련된 CSS 속성을 정의
+ */
+const responsiveProperties = defineProperties({
+    conditions : {
+        mobile: {},
+        tablet: { "@media": "screen and (min-width: 768px)" },
+        desktop: { "@media": "screen and (min-width: 1024px)" }
+    },
+    defaultCondition : 'desktop',
+    properties: {
+        display : ['none', 'block', 'inline-block', 'flex', 'grid'],
+        flexDirection : ['row', 'column'],
+        justifyContent : ['center', 'flex-start', 'flex-end', 'space-between', 'space-around'],
+        alignItems : ['center', 'flex-start', 'flex-end', 'stretch'],
+        placeItems : ['center', 'flex-start', 'flex-end', 'stretch'],
+    }
+});
+
+/**
+ * colorProperties : 색상과 관련된 CSS 속성을 정의
+ */
+const colorProperties = defineProperties({
+    conditions : {
+        lightMode : {},
+    },
+    defaultCondition : 'lightMode',
+    properties: {
+        color : colors,
+        background : colors
+    }
+});
+
+/**
+ * typeScaleProperties : 텍스트와 관련된 CSS 속성을 정의
+ */
+const typeScaleProperties = defineProperties({
+    properties: {
+        fontSize : fontSizes,
+        fontWeight : fontWeights
+    }
+})
+
+
+/**
+ * sprinkles : Sprinkle 객체
+ */
+export const sprinkles = createSprinkles(
+    responsiveProperties,
+    colorProperties,
+    typeScaleProperties
+);
+
+/**
+ * Sprinkles : Sprinkle 객체 타입
+ */
+export type Sprinkles = Parameters<typeof sprinkles>[0];
---
+++ src/styles/theme.css.ts
@@ -0,0 +1,15 @@
+import { createGlobalTheme } from "@vanilla-extract/css";
+import { colors, shadows } from "./token";
+import { futura, pretendard } from "./global/globalText.css";
+
+export const vars = createGlobalTheme(':root', {
+    // design system color(neutral, blue, red, yellow, green, teal)
+    colors,
+    // design system shadow(small, medium, large, xlarge)
+    shadows,
+    // typography 사용이 아닌 css 변수로 사용이 필요할 경우 -- 미권장
+    font : {
+        basic : pretendard,
+        logo : futura
+    }
+})
