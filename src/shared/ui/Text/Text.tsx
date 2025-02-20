3bf85feeeca19ad5d1292956b8b834101a8795a1
SeHyun <rla6170@naver.com>
2025-02-20 16:21:34 +0900

    hotfix: gitignore 반영을 위한 cached rm

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -1,21 +1,33 @@
-import { colors, fontSizes } from "@/app/token";
-import { Box } from "../Box";
+import { colors, fontSizes } from '@/app/token'
+import { Box } from '../Box'
 
-type ValidElements = "p" | "span" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+type ValidElements = 'p' | 'span' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 
 interface TextProps {
-    as?: ValidElements;
-    children?: React.ReactNode;
-    fontSize: keyof typeof fontSizes;
-    color: keyof typeof colors;
-    className?: string;
-    style? : unknown;
+    as?: ValidElements
+    children?: React.ReactNode
+    fontSize: keyof typeof fontSizes
+    color: keyof typeof colors
+    className?: string
+    style?: unknown
 }
 
-export const Text = ({as = "span", children, fontSize, color, className, style}: TextProps) => {
+export const Text = ({
+    as = 'span',
+    children,
+    fontSize,
+    color,
+    className,
+    style,
+}: TextProps) => {
     return (
-        <Box as={as} fontSize={fontSize} color={color} className={`${className} ${style}`}>
+        <Box
+            as={as}
+            fontSize={fontSize}
+            color={color}
+            className={`${className} ${style}`}
+        >
             {children}
         </Box>
-    );
+    )
 }
