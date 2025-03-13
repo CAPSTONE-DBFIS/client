bcd58a3b3633af7d590bbeb2c8c48d1d463cb959
SeHyun <rla6170@naver.com>
2025-03-13 11:34:51 +0900

    feat: userNav button 컴포넌트로 변경

--- src/features/nav/unlogin/UnLoginedUserNav.tsx
+++ src/features/nav/unlogin/UnLoginedUserNav.tsx
@@ -1,11 +1,16 @@
 import { Box } from '@/shared/ui/Box'
 import { btnBox } from './unloginedusernav.css'
+import { Button } from '@/shared/ui/Button/Button'
 
 export const UnLoginedUserNav: React.FC = () => {
     return (
-        <Box display="flex" className={btnBox}>
-            <button>로그인</button>
-            <button>회원가입</button>
+        <Box display="flex" className={btnBox} alignItems="center">
+            <Button size="small" type="primary" fontSize="body">
+                로그인
+            </Button>
+            <Button size="small" type="tertiary">
+                회원가입
+            </Button>
         </Box>
     )
 }
