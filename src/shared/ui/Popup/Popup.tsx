4d5d0a5390166993fed1ae67f7571fa3fbe2001f
SeHyun <rla6170@naver.com>
2025-03-19 09:53:03 +0900

    feat: 외부 클릭 컨트롤 커스텀 훅 제작 및 popup ref 형태로 변환

--- src/shared/ui/Popup/Popup.tsx
+++ src/shared/ui/Popup/Popup.tsx
@@ -1,5 +1,5 @@
 // react
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, forwardRef } from 'react'
 // component
 import { Box } from '../Box'
 // css
@@ -9,41 +9,40 @@ import { PopupProps } from '@/shared/types/popup.types'
 
 /**
  * Popup 컴포넌트
- * @param {React.ReactNode} chidlren children(optional)
+ * @param {React.ReactNode} children children(optional)
  * @param {usePopup} config Popup 설정 객체(required)
  * @param {number} TOP css - position - top(optional)
  * @param {number} LEFT css - position - left(optional)
  * @returns {JsxElement}
  */
-export const Popup: React.FC<PopupProps> = ({
-    children,
-    config,
-    TOP = 0,
-    LEFT = 0,
-}) => {
-    const timerRef = useRef<number | null>(null) // timeout 설정 ref
-    useEffect(() => {
-        if (config.type === 'timer') {
-            if (timerRef.current) {
-                clearTimeout(timerRef.current)
-            } // timeRef 초기화
-            timerRef.current = setTimeout(() => {
-                config.handleClose()
-            }, config.SET_TIMER_MES) // timeout 설정
-        }
-        return () => {
-            if (timerRef.current) clearTimeout(timerRef.current) // 언마운트시 제거
-        }
-    }, [config])
-    if (!config.open) return null
-    return (
-        <Box style={{ position: 'relative' }}>
-            <Box
-                className={popupArrow[config.arrow]}
-                style={{ top: `${TOP}px`, left: `${LEFT}px` }}
-            >
-                {children}
+export const Popup = forwardRef<HTMLDivElement, PopupProps>(
+    ({ children, config, TOP = 0, LEFT = 0 }, ref) => {
+        const timerRef = useRef<number | null>(null) // timeout 설정 ref
+        useEffect(() => {
+            if (config.type === 'timer') {
+                if (timerRef.current) {
+                    clearTimeout(timerRef.current)
+                } // timeRef 초기화
+                timerRef.current = setTimeout(() => {
+                    config.handleClose()
+                }, config.SET_TIMER_MES) // timeout 설정
+            }
+            return () => {
+                if (timerRef.current) clearTimeout(timerRef.current) // 언마운트시 제거
+            }
+        }, [config])
+
+        if (!config.open) return null
+
+        return (
+            <Box style={{ position: 'relative' }} ref={ref}>
+                <Box
+                    className={popupArrow[config.arrow]}
+                    style={{ top: `${TOP}px`, left: `${LEFT}px` }}
+                >
+                    {children}
+                </Box>
             </Box>
-        </Box>
-    )
-}
+        )
+    }
+)
