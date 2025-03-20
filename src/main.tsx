5b13af3782ee851f35fc137f18bcede3a9c621df
SeHyun <rla6170@naver.com>
2025-03-20 10:13:45 +0900

    feat: StrictMode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // css
 import './app/styles'
 // routing
 import AppRouter from './app/routes/AppRouter'
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <AppRouter />
-    </StrictMode>
-)
+createRoot(document.getElementById('root')!).render(<AppRouter />)
