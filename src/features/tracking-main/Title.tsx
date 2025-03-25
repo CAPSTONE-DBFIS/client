e438f22a7d1ceae30a634103534629dfbca609fe
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-03-26 01:22:52 +0900

    feat: 프로젝트 헤더 ui 구현 [DF-82]

--- src/features/tracking-main/Title.tsx
+++ src/features/tracking-main/Title.tsx
@@ -1,6 +1,74 @@
+//component
 import { Box } from '@/shared/ui/Box'
+//icons
+import Menu from '@/shared/asset/icon/menu.svg?react'
+import Right from '@/shared/asset/icon/cheveron-right.svg?react'
+import Pencil from '@/shared/asset/icon/pencil.svg?react'
+import Trash from '@/shared/asset/icon/trash.svg?react'
+//css
 import * as style from './title.css'
+import { colors } from '@/app/token'
+/**
+ * 프로젝트 헤더 컴포넌트
+ * 프로젝트 제목과 관련 메뉴를 표시하는 컴포넌트입니다.
+ * @returns {JSX.Element} Title 컴포넌트
+ */
 export const Title = () => {
-    // 메인 콘텐츠 영역 중 프로젝트 헤더
-    return <Box className={style.layout}></Box>
+    return (
+        <Box display="flex" flexDirection="column" className={style.layout}>
+            <Box display="flex" flexDirection="column" style={{ gap: '8px' }}>
+                <Box
+                    display="flex"
+                    alignItems="center"
+                    fontSize="title1"
+                    color={'neutral-900'}
+                    style={{ gap: '8px', fontWeight: '500' }}
+                >
+                    슈퍼 프로젝트
+                    <Menu width={26} height={26} />
+                </Box>
+                <Box
+                    display="flex"
+                    alignItems="center"
+                    fontSize="headline"
+                    color={'neutral-60'}
+                    style={{ gap: '4px' }}
+                >
+                    <Right
+                        width={16}
+                        height={16}
+                        fill={`${colors['neutral-60']}`}
+                    />
+                    프로젝트
+                </Box>
+            </Box>
+
+            <Box display="flex" fontSize="body" style={{ gap: '20px' }}>
+                <Box display="flex" alignItems="center" color={'neutral-50'}>
+                    {' '}
+                    <Pencil
+                        width={14}
+                        height={14}
+                        fill={`${colors['neutral-50']}`}
+                        style={{
+                            marginRight: '4px',
+                        }}
+                    />{' '}
+                    프로젝트 명 수정
+                </Box>
+                <Box display="flex" alignItems="center" color={'neutral-50'}>
+                    {' '}
+                    <Trash
+                        width={14}
+                        height={14}
+                        fill={`${colors['neutral-50']}`}
+                        style={{
+                            marginRight: '4px',
+                        }}
+                    />{' '}
+                    프로젝트 삭제
+                </Box>
+            </Box>
+        </Box>
+    )
 }
--- src/features/tracking-main/title.css.ts
+++ src/features/tracking-main/title.css.ts
@@ -1,7 +1,7 @@
 import { style } from '@vanilla-extract/css'
 
 export const layout = style({
-    height: '85px',
-    marginBottom: '16px',
-    border: '1px solid black',
+    height: '82px',
+    // border: '1px solid black',
+    gap: '16px',
 })
