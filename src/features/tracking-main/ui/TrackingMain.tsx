bf4dc9762d1575171a53b33526de57365f39f9c5
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-03-31 12:09:43 +0900

    fix(tracking-main): 파일, 인터페이스 이름 변경

--- src/features/tracking-main/ui/TrackingMain.tsx
+++ src/features/tracking-main/ui/TrackingMain.tsx
@@ -8,13 +8,13 @@ import { Dashboard } from '../dashboard/index'
 import * as style from './styles/trackingmain.css'
 
 //interface
-import { Iproject } from '../model/project.type'
+import { IProject } from '../model/project.type'
 /**
  * 메인 콘텐츠 영역(Title, Tap, Dashboard)
  *
  * @returns {JSX.Element}
  */
-export const TrackingMain = ({ projectName, projectPath }: Iproject) => {
+export const TrackingMain = ({ projectName, projectPath }: IProject) => {
     return (
         <Box className={style.layout}>
             <Title projectName={projectName} projectPath={projectPath}></Title>
