465d4a4fcc75a337549d6a93fa3594036b750e23
SeHyun <rla6170@naver.com>
2025-04-06 13:26:04 +0900

    feat: home insight component(no graph UI) [DF-93]

---
+++ src/features/home/ui/insight/Insight.css.ts
@@ -0,0 +1,22 @@
+import { colors } from '@/app/token'
+import { style } from '@vanilla-extract/css'
+
+export const container = style({
+    width: '1024px',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '24px',
+
+    backgroundColor: colors['neutral-10'],
+
+    borderRadius: '20px',
+
+    padding: '24px 82px',
+})
+
+export const logoSpan = style({
+    padding: '6px 7px',
+    backgroundColor: colors['teal-500'],
+    color: colors.white,
+    borderRadius: '6px',
+})
--- src/features/home/ui/insight/InsightGraph.tsx
+++ src/features/home/ui/insight/InsightGraph.tsx
@@ -1,19 +1,28 @@
 import { Box } from '@/shared/ui/Box'
 
 import Chip from '@/shared/asset/icon/chip.svg?react'
+import Calendar from '@/shared/asset/icon/calendar.svg?react'
 import { Text } from '@/shared/ui/Text'
 import { Button } from '@/shared/ui/Button'
 import { Select } from '@/shared/ui/Select/Select'
 import { useState } from 'react'
+import { colors } from '@/app/token'
 
 export const InsightGraph = () => {
-    const [week, setWeek] = useState('')
+    const [week, setWeek] = useState('1주')
     return (
         <Box>
             <Select
                 options={['1주', '1달', '준비중']}
                 currentValue={week}
                 setValue={setWeek}
+                Icon={
+                    <Calendar
+                        width={15}
+                        height={15}
+                        fill={colors['teal-500']}
+                    />
+                }
             />
             <Box display="flex" alignItems="center" style={{ gap: '6px' }}>
                 <Chip />
