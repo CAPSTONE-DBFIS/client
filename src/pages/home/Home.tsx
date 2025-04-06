0c9f82e17c5176c3c9bff21d5ccb59e6d05e0e20
SeHyun <rla6170@naver.com>
2025-04-06 13:25:07 +0900

    feat: home widgets components [DF-93]

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,21 @@
+import { Analysis } from '@/features/home/ui/analysis/Analysis'
+import { Article } from '@/features/home/ui/article/Article'
+import { HomeWidget } from '@/features/home/ui/widget/HomeWidget'
+import { Insight } from '@/features/home/ui/insight/Insight'
+import { Box } from '@/shared/ui/Box'
+
 export function Home() {
-    return <>HOME</>
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            style={{ gap: '64px', padding: '36px' }}
+        >
+            <HomeWidget />
+            <Article />
+            <Analysis />
+            <Insight />
+        </Box>
+    )
 }
