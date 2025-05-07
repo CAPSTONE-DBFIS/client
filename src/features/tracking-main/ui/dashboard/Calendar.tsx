2fffb16c47b4a51c96d9cb79f8e842ef3cf9a64b
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-05-07 16:04:15 +0900

    feat: FullCalendar 에 작업 추가

--- src/features/tracking-main/ui/dashboard/Calendar.tsx
+++ src/features/tracking-main/ui/dashboard/Calendar.tsx
@@ -1,74 +1,53 @@
-//compontents
-import { Box } from '@/shared/ui/Box'
-import { Text } from '@/shared/ui/Text'
-//icon
-import Left from '@/shared/asset/icon/cheveron-left.svg?react'
-import Right from '@/shared/asset/icon/cheveron-right.svg?react'
-import { useState } from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import { tasks } from '@/features/tracking-main/const/tasks'
 /**
  * 대시보드 중 캘린더
- *
+ * @param {string} dateStr - 변환하려는 날짜
+ * @constant {string[]} COLORS - 작업 색상 배열
+ * @property {string} events[].id - 작업 ID
+ * @property {string} events[].title - 제목
+ * @property {string} events[].start - 시작 날짜
+ * @property {string} events[].end - 종료 날짜
+ * @property {string} events[].color - 이벤트 색상
  * @returns {JSX.Element}
  */
+
 export const Calendar = () => {
-    const [currentDate, setCurrentDate] = useState(new Date())
-
-    const year = currentDate.getFullYear()
-    const month = currentDate.getMonth()
-
-    // 달력 시작 날짜를 현재 달의 첫 날의 주의 일요일로 설정
-    const startDay = new Date(
-        year,
-        month,
-        1 - new Date(year, month, 1).getDay()
-    )
-
-    // 현재 달의 마지막 날
-    const endDay = new Date(
-        year,
-        month + 1,
-        6 - new Date(year, month, 1).getDay()
-    )
-
-    // 달 이동 함수
-    const handleMonth = (date: number) => {
-        setCurrentDate(
-            new Date(
-                currentDate.getFullYear(),
-                currentDate.getMonth() + date,
-                1
-            )
-        )
+    //2025.03.03 -> 2025-03-03
+    const parseDate = (dateStr: string) => {
+        const parts = dateStr.split('.').map((s) => parseInt(s, 10))
+        const date = new Date(parts[0], parts[1] - 1, parts[2])
+        return date.toISOString().split('T')[0]
+    }
+    //endDate 날짜 포함
+    const addOneDay = (dateStr: string) => {
+        const date = new Date(dateStr)
+        date.setDate(date.getDate() + 1)
+        return date.toISOString().split('T')[0]
     }
 
-    const monthNames = [
-        '01',
-        '02',
-        '03',
-        '04',
-        '05',
-        '06',
-        '07',
-        '08',
-        '09',
-        '10',
-        '11',
-        '12',
-    ]
+    const COLORS = [`#E6F0FF`, '#f9c9c9', '#EBEDF0', '#f9e0c9', '#DBE7E9']
+
+    //FullCalendar에 전달할 데이터 생성
+    const events = tasks.map((task, index) => ({
+        id: task.id,
+        title: ' ',
+        start: parseDate(task.startDate),
+        end: addOneDay(parseDate(task.endDate)),
+        color: COLORS[index % COLORS.length],
+    }))
+
     return (
-        <Box>
-            <Box as={'button'} onClick={() => handleMonth(+1)}>
-                {' '}
-                <Left />
-            </Box>
-            <Text>
-                {year}.{monthNames[month]}
-            </Text>
-            <Box as={'button'} onClick={() => handleMonth(-1)}>
-                <Right />
-            </Box>
-            <Box>{startDay.toDateString()}</Box>
-            <Box>{endDay.toDateString()}</Box>
-        </Box>
+        <FullCalendar
+            plugins={[dayGridPlugin, interactionPlugin]}
+            initialView="dayGridMonth"
+            events={events}
+            eventDisplay="block"
+            height="auto"
+            dayMaxEventRows
+            fixedWeekCount={false}
+        />
     )
 }
