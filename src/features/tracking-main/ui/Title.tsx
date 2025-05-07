8f99b897cdaad39c45d97962cf27a92baff5f9e9
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-05-08 03:42:21 +0900

    feat: 프로젝트 내 경로 UI추가

--- src/features/tracking-main/ui/Title.tsx
+++ src/features/tracking-main/ui/Title.tsx
@@ -1,12 +1,13 @@
 //component
 import { Box } from '@/shared/ui/Box'
-//icons
-import Menu from '@/shared/asset/icon/menu.svg?react'
+//icon
+import Right from '@/shared/asset/icon/cheveron-right.svg?react'
 //css
 import * as style from './styles/title.css'
 //interface
 import { IProject } from '../types/project.type'
 import { Text } from '@/shared/ui/Text'
+import { colors } from '@/app/token'
 /**
  * 프로젝트 헤더 컴포넌트
  * 프로젝트 제목과 경로 표시 & 관련 메뉴(수정,삭제)를 표시하는 컴포넌트입니다.
@@ -23,9 +24,46 @@ export const Title = ({ projectName }: IProject) => {
                     color={'neutral-900'}
                     style={{ gap: '8px', fontWeight: '500' }}
                 >
-                    <Menu width={26} height={26} />
-
-                    <Text fontSize="title1">{projectName}</Text>
+                    <Text fontSize="title1" fontWeight="semibold">
+                        {projectName}
+                    </Text>
+                </Box>
+                <Box>
+                    <Box
+                        display="flex"
+                        justifyContent="flex-start"
+                        alignItems="center"
+                        className={style.teamWrapper}
+                    >
+                        <Box
+                            display="flex"
+                            alignItems="center"
+                            className={style.team}
+                        >
+                            <Right
+                                width={17}
+                                height={17}
+                                fill={colors['neutral-60']}
+                            />
+                            <Text fontSize="body" color={'neutral-60'}>
+                                00팀
+                            </Text>
+                        </Box>
+                        <Box
+                            display="flex"
+                            alignItems="center"
+                            className={style.team}
+                        >
+                            <Right
+                                width={17}
+                                height={17}
+                                fill={colors['neutral-60']}
+                            />
+                            <Text fontSize="body" color={'neutral-60'}>
+                                00프로젝트
+                            </Text>
+                        </Box>
+                    </Box>
                 </Box>
             </Box>
         </Box>
--- src/features/tracking-main/ui/styles/title.css.ts
+++ src/features/tracking-main/ui/styles/title.css.ts
@@ -3,3 +3,12 @@ import { style } from '@vanilla-extract/css'
 export const layout = style({
     // border: '1px solid black',
 })
+
+export const teamWrapper = style({
+    paddingTop: '8px',
+    gap: '8px',
+})
+
+export const team = style({
+    gap: '4px',
+})
